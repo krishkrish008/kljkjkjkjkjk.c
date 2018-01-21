694845e58a9961ddea7a3be71c5d55674c09c0d0
krishkrish008 <35235157+krishkrish008@users.noreply.github.com>
2018-01-21 16:29:58 +0530

    Create uuuu.c

---
+++ uuuu.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+int a,b,c;
+printf("Enter value upto sum has to be calculated");
+scanf("%d",&a);
+b=a*a+a;
+c=b/2;
+printf("sum upto nth term is%d",c);
+return 0;
+}
